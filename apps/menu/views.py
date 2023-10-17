11a1dbc0a8ec2c0e82677736aafad87dc775ae24
bezbak <Cemetey.2019@gmail.com>
2023-10-17 18:48:53 +0600

    add categories and products

--- apps/menu/views.py
+++ apps/menu/views.py
@@ -5,11 +5,13 @@ from .models import Category, Product
 
 def index(request):
     categories = Category.objects.all()
-    products = Product.objects.all()[:8]
+    products = Product.objects.all().filter(category_id=1)[:8]
+    category = Category.objects.get(id=1)
     ads = ADS.objects.all().latest('id')
     context = {
         'categories':categories,
         'products':products,
         'ads':ads,
+        'category':category,
     }
     return render(request, 'index.html',context)
